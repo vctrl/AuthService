3128b989d20e4bb32684ea47b693fe24bcd46f6e
Viktor Shutov <vectoreal@gmail.com>
2020-12-28 13:01:35 +0300

    Added case for invalid state and refactoring

--- delivery/handler_test.go
+++ delivery/handler_test.go
@@ -14,7 +14,9 @@ import (
 )
 
 const (
-	testState    = "test_state"
+	testState  = "test_state"
+	testState2 = "test_state2"
+
 	testSite     = "test_site"
 	testAuthCode = "test_auth_code"
 )
@@ -51,35 +53,18 @@ func TestLoginHappyCase(t *testing.T) {
 
 func TestCallbackNoCookieReturnError(t *testing.T) {
 	r, uc := createTestContext()
-	reqPath := fmt.Sprintf("/callback?state=%s", testState)
-	req, _ := http.NewRequest("GET", reqPath, nil)
-
-	q := req.URL.Query()
-	q.Add("state", testState)
-	q.Add("site", testSite)
-	q.Add("code", testAuthCode)
-	req.URL.RawQuery = q.Encode()
+	req := createRequest(testState, testState, false)
 
 	uc.On("Callback", testSite, testAuthCode).Return(&usecase.Response{}, nil)
 
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
-	fmt.Println(w)
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 }
 
 func TestCallbackUsecaseReturnError(t *testing.T) {
 	r, uc := createTestContext()
-	reqPath := fmt.Sprintf("/callback?state=%s", testState)
-	req, _ := http.NewRequest("GET", reqPath, nil)
-	cookie := &http.Cookie{Name: oauthStateHeader, Value: testState}
-	req.AddCookie(cookie)
-
-	q := req.URL.Query()
-	q.Add("state", testState)
-	q.Add("site", testSite)
-	q.Add("code", testAuthCode)
-	req.URL.RawQuery = q.Encode()
+	req := createRequest(testState, testState, true)
 
 	uc.On("Callback", testSite, testAuthCode).Return(&usecase.Response{}, errors.New("failed"))
 
@@ -90,22 +75,26 @@ func TestCallbackUsecaseReturnError(t *testing.T) {
 
 func TestCallbackHappyCase(t *testing.T) {
 	r, uc := createTestContext()
-	reqPath := fmt.Sprintf("/callback?state=%s", testState)
-	req, _ := http.NewRequest("GET", reqPath, nil)
-	cookie := &http.Cookie{Name: oauthStateHeader, Value: testState}
-	req.AddCookie(cookie)
-
-	q := req.URL.Query()
-	q.Add("state", testState)
-	q.Add("site", testSite)
-	q.Add("code", testAuthCode)
-	req.URL.RawQuery = q.Encode()
+	req := createRequest(testState, testState, true)
 
 	uc.On("Callback", testSite, testAuthCode).Return(&usecase.Response{}, nil)
 
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestCallbackInvalidStateReturnError(t *testing.T) {
+	r, uc := createTestContext()
+
+	// states doesn't match
+	req := createRequest(testState, testState2, true)
+
+	uc.On("Callback", testSite, testAuthCode).Return(&usecase.Response{}, nil)
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
 }
 
 func createTestContext() (*gin.Engine, *usecase.OAuthUseCaseMock) {
@@ -117,4 +106,22 @@ func createTestContext() (*gin.Engine, *usecase.OAuthUseCaseMock) {
 	uc := new(usecase.OAuthUseCaseMock)
 	RegisterHTTPEndpoints(r, uc)
 	return r, uc
+}
+
+func createRequest(testState1, testState2 string, withCookie bool) *http.Request {
+	reqPath := fmt.Sprintf("/callback?state=%s", testState)
+	req, _ := http.NewRequest("GET", reqPath, nil)
+
+	q := req.URL.Query()
+	q.Add("state", testState1)
+	q.Add("site", testSite)
+	q.Add("code", testAuthCode)
+	req.URL.RawQuery = q.Encode()
+
+	if withCookie {
+		cookie := &http.Cookie{Name: oauthStateHeader, Value: testState2}
+		req.AddCookie(cookie)
+	}
+
+	return req
 }
